398b1cbbb055d0cc60f9855be2aa7f5e4343dc3f
Olivier Janssens <oj218@dhcp-89.caret.private>
2010-06-04 11:26:56 +0100

    igoogle - Recentmessages & profile implemented. JSLINT valid

---
+++ devwidgets/igoogle/README.rtf
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1038\cocoasubrtf290
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\froman\fcharset0 TimesNewRomanPSMT;}
+{\colortbl;\red255\green255\blue255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww9000\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\ql\qnatural\pardirnatural
+
+\f0\fs24 \cf0 To deploy the igoogle widget: \
+\
+Because the igoogle widget can't work from the localhost an xml file has to be uploaded to a online domain. Though this is not necessary for a deployed Sakai. The only thing that has to change is "
+\f1\fs20 <Content type="url" href="http://localhost:8080/devwidgets/igoogle/igoogle.html" /> 
+\f0\fs24 ' in the xml file. To actually deploy the widget on igoogle you'll need to add the "My gadgets" widget on igoogle and upload the xml file. When this is done the widget is deployed.\
+\
+A copy of the XML file can be found in this folder. Though currently for development there's one on www.ojtwist.be.}
--- devwidgets/igoogle/javascript/igoogle.js
+++ devwidgets/igoogle/javascript/igoogle.js
@@ -15,6 +15,8 @@
 * KIND, either express or implied. See the License for the
 * specific language governing permissions and limitations under the License.
 */
+/*global Config, fluid, $*/
+
 
 var sakai = sakai || {};
 
@@ -32,6 +34,10 @@ sakai.igoogle = function(){
     var $igUsername = $('#w7_username');
     var $igHeader = $('#ig_header');
 
+    // Header
+    var $igProfileHeader = $("#ig_profile");
+    var $igRecentMessageButton = $('#ig_recent_message_button');
+
     // Recent messages
     var $igRecentMessages = $('#ig_recent_messages');
     var $igShort = $(".ig_short");
@@ -39,8 +45,19 @@ sakai.igoogle = function(){
     var $igUserRepeat = $('.ig_user_repeat');
     var $igMessage = $('#ig_message');
 
+    // Message 
+    var $igBackuBtton = $('#ig_back_button');
+    var $igSubject = $('#ig_subject');
+
+    // Profile
+    var $igProfile = $('#ig_profile_tab');
+
     // Error messages
     var $igLoginFailed = $('#ig_login_failed');
+    var $igNoMessage = $('#ig_no_message');
+
+    // Logout
+    var $igLogoutButton = $('#ig_logout');
 
     // CSS classes
     var changeColorNormal = 'ig_change_color_normal';
@@ -56,24 +73,43 @@ sakai.igoogle = function(){
     // Templates
     var $igRecentmessagesTemplate = $('#ig_recent_messages_template');
     var $igMessageTemplate = $('#ig_message_template');
+    var $igProfileTemplate = $('#ig_profile_template');
+
+     /**
+     * Check whether there is a valid picture for the user
+     * @param {Object} profile The profile object that could contain the profile picture
+     * The complete URL of the profile picture
+     * Will be an empty string if there is no picture
+     */
+    var constructProfilePicture = function(profile){
+        if (profile.picture && profile.path) {
+            return "/_user" + profile.path + "/public/profile/" + $.parseJSON(profile.picture).name;
+        }
+        else {
+            return "/dev/_images/person_icon.jpg";
+        }
+    };
 
     /**
-     * This will determine whether there is a valid session. If there is, we'll
-     * redirect to the URL requested or the personal dashboard if nothing has been provided.
+     * Show the profile information of the loggedin user
      */
-    var decideLoggedIn = function(data){
+    var requestProfileInformation = function(){
+        sakai.api.User.loadMeData(function(success, data){
+                if (success) {
+                    var profile = data;
+                    profile.userPicture = constructProfilePicture(data);
+                    $igProfile.hide();
+                    $igProfile.html($.TemplateRenderer($igProfileTemplate,profile));
+                }
+            });
+    };
 
-        $igLoginFailed.hide();
-
-        var mejson = (data === undefined ? sakai.data.me : data);
-        if (mejson.user.userid) {
-            currentUser = mejson;
-            $igLoginContainer.hide();
-            $igHeader.show();
-            requestRecentMessages();
-        } else {
-            $igLoginFailed.show();
-        }
+    /**
+     * This function will display the list of recentmessages when the user is in the message view
+     */
+    var showRecentMessageList = function(){
+        $igMessage.hide();
+        $igRecentMessages.show();
     };
 
     /**
@@ -82,19 +118,29 @@ sakai.igoogle = function(){
     var showMessage = function(){
 
         $igRecentMessages.hide();
-        $that = $(this);
+        var $that = $(this);
         $igHidden = $('.s3d-hidden');
         $igUserRepeat = $('.ig_user_repeat');
 
+        // Construct the message object
         var messageObj = {
-            'sender': $($igUserRepeat, $(this).parent()).html(),
-            "subject": $($igHidden, $(this).parent()).html(),
-            "message": $('p', $(this).parent()).html()
+            'sender': $($igUserRepeat, $that.parent()).html(),
+            "subject": $($igHidden, $that.parent()).html(),
+            "message": $('p', $that.parent()).html()
         };
 
+        //Render the message in html
         $igMessage.html($.TemplateRenderer($igMessageTemplate,messageObj));
+        $igMessage.show();
+        $igBackuBtton = $('#ig_back_button');
+        $igBackuBtton.bind('click',showRecentMessageList);
 
+        $igSubject = $('#ig_subject');
 
+        // Make sure no subject is longer than 80 chars
+        $igSubject.each(function(){
+              $(this).html($(this).html().substring(0, 80) + "...");
+        });
     };
 
     /** 
@@ -103,8 +149,15 @@ sakai.igoogle = function(){
      */
     var displayRecentMessages = function(data){
 
+        $igNoMessage.hide();
+        $igProfile.hide();
+
         // Check if there are any messages
         if (data.results.length) {
+
+            $igRecentMessages.show();
+            $igProfile.hide();
+            $igMessage.hide();
 
             //If there are more than 7 messages, only show the last 7
             if(data.results.length > 7){
@@ -119,6 +172,8 @@ sakai.igoogle = function(){
             //$macRecentMessages = $('#mac_recentMessages');
             $("a", $igRecentMessages).bind('click', showMessage);
 
+            $igShort = $(".ig_short");
+
             //Make sure the subject isn't longer than the div is wide
             $igShort.each(function(){
                 if ($(this).html().length > 10) {
@@ -127,9 +182,53 @@ sakai.igoogle = function(){
             });
         }else{
             //If there are no messages show an error messages
-            $macNoMessage.show();
+            $igNoMessage.show();
         }
+    };
 
+    /**
+     * This function will do an Ajax call to get the messages from the server
+     */
+    var requestRecentMessages = function(){
+
+      var params = $.param({
+            box: "inbox",
+            category: "message",
+            sortOn: "sakai:created",
+            sortOrder: "descending"
+        });
+
+        $.ajax({
+            url: sakai.config.URL.MESSAGE_BOXCATEGORY_SERVICE + "?" + params,
+            success: function(data){
+                displayRecentMessages(data);
+            },
+            error: function(xhr, textStatus, thrownError){
+                fluid.log("Error at the request for new images after the drag and drop");
+            }
+        });
+    };
+
+    /**
+     * This will determine whether there is a valid session. If there is, we'll
+     * redirect to the URL requested or the personal dashboard if nothing has been provided.
+     * @param data, the response
+     */
+    var decideLoggedIn = function(data){
+
+        $igLoginFailed.hide();
+
+        // Check if user data is returned, if get the profile information & recentmessages
+        var mejson = (data === undefined ? sakai.data.me : data);
+        if (mejson.user.userid) {
+            currentUser = mejson;
+            $igLoginContainer.hide();
+            $igHeader.show();
+            requestProfileInformation();
+            requestRecentMessages();
+        } else {
+            $igLoginFailed.show();
+        }
     };
 
      /**
@@ -176,7 +275,7 @@ sakai.igoogle = function(){
             type: "POST",
             success: checkLogInSuccess,
             error: function(){
-                alert('error');
+                fluid.log('error at the performlogin');
             },
             data: data
         });
@@ -219,41 +318,65 @@ sakai.igoogle = function(){
     };
 
     /**
-     * This function will do an ajac call to get the messages from the server
-     * @param {String} token
-     * @exaple saveToken(XWFiZDvAv428m5eMnnbcyWWwY38=;admin)
+     * This function will control the style of the a tags in the header
      */
-    var requestRecentMessages = function(){
-
-      var params = $.param({
-            box: "inbox",
-            category: "message",
-            sortOn: "sakai:created",
-            sortOrder: "descending"
-        });
-
-        $.ajax({
-            url: sakai.config.URL.MESSAGE_BOXCATEGORY_SERVICE + "?" + params,
-            success: function(data){
-                displayRecentMessages(data);
-            },
-            error: function(xhr, textStatus, thrownError){
-                fluid.log("Error at the request for new images after the drag and drop");
-            }
-        });
-    };
-
     var activateLink = function(){
         $('a',$igHeader).removeClass(activelink);
         $('a',$igHeader).addClass(inactivelink);
-
         $(this).removeClass(inactivelink);
         $(this).addClass(activelink);
     };
 
+    /**
+     * This function will show the profile information of the user
+     */
+    var showProfileInformation = function(){
+        $igProfile = $('#ig_profile_tab');
+        $igNoMessage.hide();
+        $igProfile.show();
+        $igRecentMessages.hide();
+        $igMessage.hide();
+    };
 
     /**
-     * This funtion
+     * This function will signout the user
+     */
+    var signout = function(){
+        var data = {
+            "sakai:status": "offline",
+            "_charset_": "utf-8"
+        };
+        $.ajax({
+            url: sakai.config.URL.PRESENCE_SERVICE,
+            type: "POST",
+            success: function(){
+                $igMessage.empty();
+                $igRecentMessages.empty();
+                $igHeader.hide();
+                $igProfile.hide();
+                $igLoginContainer.show();
+            },
+            data: data
+        });
+    };
+
+    /**
+     * This function will logout the user
+     */
+    var logout = function(){
+        $.ajax({
+            url: sakai.config.URL.LOGOUT_SERVICE,
+            type: "POST",
+            complete: signout,
+            data: {
+                "sakaiauth:logout": "1",
+                "_charset_": "utf-8"
+            }
+        });
+    };
+
+    /**
+     * This funtion will bind all the events
      */
     var bindEvents = function(){
 
@@ -275,20 +398,23 @@ sakai.igoogle = function(){
             changeColour($igUsername,defaultvalue);
         });
 
-        $('#ig_recent_message_button').bind('click',requestRecentMessages);
+        $igRecentMessageButton.bind('click',requestRecentMessages);
 
-        $('#ig_recent_message_button').bind('focus',activateLink);
+        $igRecentMessageButton.bind('focus',activateLink);
 
-        $("#ig_profile").bind('focus',activateLink);
+        $igProfileHeader.bind('focus',activateLink);
+        $igProfileHeader.bind('click',showProfileInformation);
+        $igLogoutButton.bind('click',logout);
     };
 
+    /** 
+     * This is the first function that'll be executed
+     */
     var init = function(){
         $igLoginContainer.submit(performLogIn);
         defaultvalue = $igUsername.val();
         defaultvalue2 = $igPassword.val();
-
         bindEvents();
-
         $('body').show();
     };
     init();
